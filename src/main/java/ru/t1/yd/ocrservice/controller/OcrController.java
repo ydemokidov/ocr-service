68b8c094133ffd80fa763d6203bfac7f433306a5
Yuriy D <ydemokidov@gmail.com>
2022-05-16 17:30:28 +0300

    Добавлен прототип контроллера

---
+++ src/main/java/ru/t1/yd/ocrservice/controller/OcrController.java
@@ -0,0 +1,34 @@
+package ru.t1.yd.ocrservice.controller;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+import ru.t1.yd.ocrservice.service.OcrService;
+
+@RestController
+@RequestMapping("ocr")
+public class OcrController {
+
+    private final OcrService ocrService;
+
+    @Autowired
+    public OcrController(@NotNull final OcrService ocrService) {
+        this.ocrService = ocrService;
+    }
+
+    @PostMapping(value = "text", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public Mono<String> processImageForText(@RequestPart("file") final Mono<FilePart> filePartMono){
+        try {
+            return Mono.just("OK!");
+        }catch (Exception e){
+            return Mono.error(e);
+        }
+    }
+
+}
