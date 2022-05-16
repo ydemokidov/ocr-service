e35ed5b69545919c9b734857918cac23261b75b0
Yuriy D <ydemokidov@gmail.com>
2022-05-16 19:46:35 +0300

    В прототип контроллера добавлен тестовый метод для чтения строки из переданного изображения

--- src/main/java/ru/t1/yd/ocrservice/controller/OcrController.java
+++ src/main/java/ru/t1/yd/ocrservice/controller/OcrController.java
@@ -1,5 +1,6 @@
 package ru.t1.yd.ocrservice.controller;
 
+import net.sourceforge.tess4j.TesseractException;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -10,6 +11,15 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 import ru.t1.yd.ocrservice.service.OcrService;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("ocr")
@@ -23,9 +33,11 @@ public class OcrController {
     }
 
     @PostMapping(value = "text", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public Mono<String> processImageForText(@RequestPart("file") final Mono<FilePart> filePartMono){
+    public Mono<String> processImageForText(@RequestPart("file") final FilePart filePart){
         try {
-            return Mono.just("OK!");
+            Path fileToCopy = Path.of("tstFile.bmp");
+            System.out.println(fileToCopy.toFile().getAbsolutePath());
+            return Mono.from(filePart.transferTo(fileToCopy)).thenReturn(ocrService.getFullStringFromImage(fileToCopy));
         }catch (Exception e){
             return Mono.error(e);
         }
